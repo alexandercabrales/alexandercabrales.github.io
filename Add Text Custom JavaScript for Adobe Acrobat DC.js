dc97a2e79eb252e492b76d23fe9732ebb47f1074
alexandercabrales <37554346+alexandercabrales@users.noreply.github.com>
2025-06-18 21:13:44 -0700

    Update Add Text Custom JavaScript for Adobe Acrobat DC.js
    adding comments

--- Add Text Custom JavaScript for Adobe Acrobat DC.js
+++ Add Text Custom JavaScript for Adobe Acrobat DC.js
@@ -1,4 +1,4 @@
-var f = this.addField("customText", "text", this.pageNum, [500, 50, 600, 80]);
+var f = this.addField("customText", "text", this.pageNum, [500, 50, 600, 80]); //     edit pageNum for text box location
 f.textFont = font.Garamond;
 f.textSize = 24;
 f.value = "Your Text Here";
