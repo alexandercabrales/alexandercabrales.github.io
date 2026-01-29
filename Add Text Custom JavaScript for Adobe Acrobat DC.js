f2c8d4a38f9c5a522a80a7996bae7d8e1f99f1f0
alexandercabrales <37554346+alexandercabrales@users.noreply.github.com>
2026-01-28 17:49:46 -0800

    Update Add Text Custom JavaScript for Adobe Acrobat DC.js
    add some info and fix some errors.

--- Add Text Custom JavaScript for Adobe Acrobat DC.js
+++ Add Text Custom JavaScript for Adobe Acrobat DC.js
@@ -1,5 +1,5 @@
-var f = this.addField("customText", "text", this.pageNum, [500, 50, 600, 80]); //     edit pageNum for text box location
+var f = this.addField("customText", "text", this.pageNum, [500, 50, 600, 80]); // adds text form field.   edit pageNum for text box location
 f.textFont = font.Garamond;
 f.textSize = 24;
-f.value = "Your Text Here";
-f.readonly = false; // Make it editable
+f.value = "Your Text Here"; // What the user has typed in as the value for the field we created in the first line
+f.readonly = false; // Makes it editable
